81d5e1e6721728ea28c37925591e81c47be1fd72
Alan Crew Editz <alancreweditzofficial@gmail.com>
2026-08-27 21:14:48 +0530

    Add files via upload

---
+++ projects.js
@@ -0,0 +1,47 @@
+window.PORTFOLIO_PROJECTS = [
+  {
+    "id": "project-1787842405695",
+    "title": "2026 A/l Physics Guessings (After Exam)",
+    "description": "Owner - DR Nagollagama\nEditor - Alan Crew",
+    "category": "Video Editing",
+    "tags": [
+      "Filmora & Photoshop"
+    ],
+    "startDate": "2026-08-23",
+    "endDate": "2026-08-23",
+    "thumbnail": "https://i.postimg.cc/pTC2RtTJ/S04-E1.png",
+    "video": "https://youtu.be/nXYM9c3NfvI?si=cH6yYRmLxGYnKw8a",
+    "github": "https://github.com/alancreweditz",
+    "demo": "https://youtu.be/nXYM9c3NfvI?si=cH6yYRmLxGYnKw8a"
+  },
+  {
+    "id": "project-1787844089487",
+    "title": "2026 Physics Guessings (Before Exam)",
+    "description": "Owner - DR Nagollagama\nEditor _ Alan Crew",
+    "category": "Video Editing",
+    "tags": [
+      "Filmora & Photoshop"
+    ],
+    "startDate": "2026-03-10",
+    "endDate": "2026-03-13",
+    "thumbnail": "https://i.postimg.cc/NFBfhkcs/S04-E2.png",
+    "video": "https://youtu.be/f7sCq1Mbta0?si=ue580-_u6Vv6dXNx",
+    "github": "https://github.com/alancreweditz",
+    "demo": "https://youtu.be/f7sCq1Mbta0?si=ue580-_u6Vv6dXNx"
+  },
+  {
+    "id": "project-1787845207058",
+    "title": "Vehicle Introduction | Promotional | Motivational",
+    "description": "Owner - DR Nagollagama\nEditor - Alan Crew",
+    "category": "Video Editing",
+    "tags": [
+      "Filmora"
+    ],
+    "startDate": "2026-03-24",
+    "endDate": "2026-03-26",
+    "thumbnail": "https://i.postimg.cc/NMJYVqp5/P1.png",
+    "video": "https://www.youtube.com/watch?v=nXYM9c3NfvI",
+    "github": "https://github.com/alancreweditz",
+    "demo": "https://www.youtube.com/watch?v=nXYM9c3NfvI"
+  }
+];
