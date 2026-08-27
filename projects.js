07882bbbcd8085c76910aa32e8d3585df8e558e3
Alan Crew Editz <alancreweditzofficial@gmail.com>
2026-08-27 20:04:43 +0530

    Add files via upload

---
+++ projects.js
@@ -0,0 +1,15 @@
+window.PORTFOLIO_PROJECTS = [
+  {
+    "id": "project-1787839920827",
+    "title": "2026 A/l Physics Anumana Video (2026/08/23)",
+    "description": "Owner - DR Nagollagama",
+    "category": "Video Editing",
+    "tags": [
+      "Filmora & Photoshop"
+    ],
+    "thumbnail": "https://i.postimg.cc/pTC2RtTJ/S04-E1.png",
+    "video": "https://youtu.be/nXYM9c3NfvI?si=FehcMBFFbsjfI8KI",
+    "github": "https://github.com/alancreweditz",
+    "demo": "https://youtu.be/nXYM9c3NfvI?si=FehcMBFFbsjfI8KI"
+  }
+];
