a81a6d4a840d15079dfc746bf36446148d2b375b
Alan Crew Editz <alancreweditzofficial@gmail.com>
2026-08-27 22:30:51 +0530

    Add files via upload

---
+++ projects.js
@@ -0,0 +1,107 @@
+window.PORTFOLIO_PROJECTS = [
+  {
+    "id": "project-1787842405695",
+    "title": "2026 A/l Physics Guessings (After Exam)",
+    "description": "Owner - DR Nagollagama\nEditor - Alan Crew",
+    "category": "Video Editing",
+    "tags": [
+      "Filmora & Photoshop"
+    ],
+    "startDate": "2026-08-23",
+    "endDate": "2026-08-23",
+    "thumbnail": "https://i.postimg.cc/pTC2RtTJ/S04-E1.png",
+    "video": "https://youtu.be/nXYM9c3NfvI?si=cH6yYRmLxGYnKw8a",
+    "github": "https://github.com/alancreweditz",
+    "demo": "https://youtu.be/nXYM9c3NfvI?si=cH6yYRmLxGYnKw8a"
+  },
+  {
+    "id": "project-1787844089487",
+    "title": "2026 Physics Guessings (Before Exam)",
+    "description": "Owner - DR Nagollagama\nEditor _ Alan Crew",
+    "category": "Video Editing",
+    "tags": [
+      "Filmora & Photoshop"
+    ],
+    "startDate": "2026-03-10",
+    "endDate": "2026-03-13",
+    "thumbnail": "https://i.postimg.cc/NFBfhkcs/S04-E2.png",
+    "video": "https://youtu.be/f7sCq1Mbta0?si=ue580-_u6Vv6dXNx",
+    "github": "https://github.com/alancreweditz",
+    "demo": "https://youtu.be/f7sCq1Mbta0?si=ue580-_u6Vv6dXNx"
+  },
+  {
+    "id": "project-1787845207058",
+    "title": "Vehicle Introduction | Promotional | Motivational",
+    "description": "Owner - DR Nagollagama\nEditor - Alan Crew",
+    "category": "Video Editing",
+    "tags": [
+      "Filmora"
+    ],
+    "startDate": "2026-03-24",
+    "endDate": "2026-03-26",
+    "thumbnail": "https://i.postimg.cc/NMJYVqp5/P1.png",
+    "video": "https://www.youtube.com/watch?v=nXYM9c3NfvI",
+    "github": "https://github.com/alancreweditz",
+    "demo": "https://www.youtube.com/watch?v=nXYM9c3NfvI"
+  },
+  {
+    "id": "project-1787848095220",
+    "title": "2026 Model Paper 33",
+    "description": "Owner - DR Nagollagama\nEditor - Alan Crew\nBatch - 2026\nSubject - Physics",
+    "category": "Video Editing",
+    "tags": [
+      "Filmora & Photoshop"
+    ],
+    "startDate": "2026-01-07",
+    "endDate": "2026-01-08",
+    "thumbnail": "https://i.postimg.cc/vTD4cvc0/P2.png",
+    "video": "https://youtu.be/YV6bxQNWvKY?si=WwywbzdZR4WSUStU",
+    "github": "https://github.com/alancreweditz",
+    "demo": "https://youtu.be/YV6bxQNWvKY?si=WwywbzdZR4WSUStU"
+  },
+  {
+    "id": "project-1787849371398",
+    "title": "Iran Riyal | Promotional",
+    "description": "Owner - DR Nagollagama\nEditor - Alan Crew\nPromotional Video",
+    "category": "Video Editing",
+    "tags": [
+      "Filmora"
+    ],
+    "startDate": "2026-01-24",
+    "endDate": "2026-01-24",
+    "thumbnail": "https://i.postimg.cc/rFX9vdb3/P4.png",
+    "video": "https://youtu.be/rgUMzb68EhM?si=3NcspEuoO75fDvTI",
+    "github": "https://github.com/alancreweditz",
+    "demo": "https://youtu.be/rgUMzb68EhM?si=7jtXaXcxLf3SI2KH"
+  },
+  {
+    "id": "project-1787849776844",
+    "title": "Day 19 | MCQ Point Class Introduction",
+    "description": "Owner - DR Nagollagama\nEditor - Alan Crew\nBatch - 2026\nSubject - Physics",
+    "category": "Video Editing",
+    "tags": [
+      "Filmora & Photoshop"
+    ],
+    "startDate": "2026-02-12",
+    "endDate": "2026-02-15",
+    "thumbnail": "https://i.postimg.cc/zGR83cpT/ද-න-ම-2026-පන-ත-වලට-සම-බන-ධ-ව-න-න-ඕන-ද-You-Tube-and-6-more-pages-Personal-Microsoft-Ed.png",
+    "video": "https://www.youtube.com/watch?v=yGj895KxkqI",
+    "github": "https://github.com/alancreweditz",
+    "demo": "https://www.youtube.com/watch?v=yGj895KxkqI"
+  },
+  {
+    "id": "project-1787849990184",
+    "title": "Model Paper 04",
+    "description": "Owner - DR Nagollagama\nEditor - Alan Crew\nBatch - 2026\nSubject - Physics",
+    "category": "Video Editing",
+    "tags": [
+      "Filmora & Photoshop"
+    ],
+    "startDate": "2026-01-07",
+    "endDate": "2026-01-08",
+    "thumbnail": "https://i.postimg.cc/WzJX0gyr/P3.png",
+    "video": "https://youtu.be/s2HXpndYaB0?si=86LmC3DtvZxLn5ud",
+    "github": "https://github.com/alancreweditz",
+    "demo": "https://youtu.be/s2HXpndYaB0?si=86LmC3DtvZxLn5ud"
+  }
+];
