bf3ec9aed80d0dc9205fbfe8892f3120613b232b
Alan Crew Editz <alancreweditzofficial@gmail.com>
2026-08-28 08:43:27 +0530

    Add files via upload

---
+++ projects.js
@@ -0,0 +1,227 @@
+window.PORTFOLIO_PROJECTS = [
+  {
+    "id": "project-1787842405695",
+    "title": "2026 A/l Physics Guessings (After Exam)",
+    "description": "Owner - DR Nagollagama\nEditor - Alan Crew",
+    "category": "Video Editing",
+    "tags": [
+      "Filmora & Photoshop"
+    ],
+    "startDate": "2026-08-23",
+    "endDate": "2026-08-23",
+    "thumbnail": "https://i.postimg.cc/pTC2RtTJ/S04-E1.png",
+    "video": "https://youtu.be/nXYM9c3NfvI?si=cH6yYRmLxGYnKw8a",
+    "github": "https://github.com/alancreweditz",
+    "demo": "https://youtu.be/nXYM9c3NfvI?si=cH6yYRmLxGYnKw8a"
+  },
+  {
+    "id": "project-1787844089487",
+    "title": "2026 Physics Guessings (Before Exam)",
+    "description": "Owner - DR Nagollagama\nEditor _ Alan Crew",
+    "category": "Video Editing",
+    "tags": [
+      "Filmora & Photoshop"
+    ],
+    "startDate": "2026-03-10",
+    "endDate": "2026-03-13",
+    "thumbnail": "https://i.postimg.cc/NFBfhkcs/S04-E2.png",
+    "video": "https://youtu.be/f7sCq1Mbta0?si=ue580-_u6Vv6dXNx",
+    "github": "https://github.com/alancreweditz",
+    "demo": "https://youtu.be/f7sCq1Mbta0?si=ue580-_u6Vv6dXNx"
+  },
+  {
+    "id": "project-1787845207058",
+    "title": "Vehicle Introduction | Promotional | Motivational",
+    "description": "Owner - DR Nagollagama\nEditor - Alan Crew",
+    "category": "Video Editing",
+    "tags": [
+      "Filmora"
+    ],
+    "startDate": "2026-03-24",
+    "endDate": "2026-03-26",
+    "thumbnail": "https://i.postimg.cc/NMJYVqp5/P1.png",
+    "video": "https://www.youtube.com/watch?v=nXYM9c3NfvI",
+    "github": "https://github.com/alancreweditz",
+    "demo": "https://www.youtube.com/watch?v=nXYM9c3NfvI"
+  },
+  {
+    "id": "project-1787848095220",
+    "title": "2026 Model Paper 33",
+    "description": "Owner - DR Nagollagama\nEditor - Alan Crew\nBatch - 2026\nSubject - Physics",
+    "category": "Video Editing",
+    "tags": [
+      "Filmora & Photoshop"
+    ],
+    "startDate": "2026-01-07",
+    "endDate": "2026-01-08",
+    "thumbnail": "https://i.postimg.cc/vTD4cvc0/P2.png",
+    "video": "https://youtu.be/YV6bxQNWvKY?si=WwywbzdZR4WSUStU",
+    "github": "https://github.com/alancreweditz",
+    "demo": "https://youtu.be/YV6bxQNWvKY?si=WwywbzdZR4WSUStU"
+  },
+  {
+    "id": "project-1787849371398",
+    "title": "Iran Riyal | Promotional",
+    "description": "Owner - DR Nagollagama\nEditor - Alan Crew\nPromotional Video",
+    "category": "Video Editing",
+    "tags": [
+      "Filmora"
+    ],
+    "startDate": "2026-01-24",
+    "endDate": "2026-01-24",
+    "thumbnail": "https://i.postimg.cc/rFX9vdb3/P4.png",
+    "video": "https://youtu.be/rgUMzb68EhM?si=3NcspEuoO75fDvTI",
+    "github": "https://github.com/alancreweditz",
+    "demo": "https://youtu.be/rgUMzb68EhM?si=7jtXaXcxLf3SI2KH"
+  },
+  {
+    "id": "project-1787849776844",
+    "title": "Day 19 | MCQ Point Class Introduction",
+    "description": "Owner - DR Nagollagama\nEditor - Alan Crew\nBatch - 2026\nSubject - Physics",
+    "category": "Video Editing",
+    "tags": [
+      "Filmora & Photoshop"
+    ],
+    "startDate": "2026-02-12",
+    "endDate": "2026-02-15",
+    "thumbnail": "https://i.postimg.cc/zGR83cpT/ද-න-ම-2026-පන-ත-වලට-සම-බන-ධ-ව-න-න-ඕන-ද-You-Tube-and-6-more-pages-Personal-Microsoft-Ed.png",
+    "video": "https://www.youtube.com/watch?v=yGj895KxkqI",
+    "github": "https://github.com/alancreweditz",
+    "demo": "https://www.youtube.com/watch?v=yGj895KxkqI"
+  },
+  {
+    "id": "project-1787849990184",
+    "title": "Model Paper 04",
+    "description": "Owner - DR Nagollagama\nEditor - Alan Crew\nBatch - 2026\nSubject - Physics",
+    "category": "Video Editing",
+    "tags": [
+      "Filmora & Photoshop"
+    ],
+    "startDate": "2026-01-07",
+    "endDate": "2026-01-08",
+    "thumbnail": "https://i.postimg.cc/WzJX0gyr/P3.png",
+    "video": "https://youtu.be/s2HXpndYaB0?si=86LmC3DtvZxLn5ud",
+    "github": "https://github.com/alancreweditz",
+    "demo": "https://youtu.be/s2HXpndYaB0?si=86LmC3DtvZxLn5ud"
+  },
+  {
+    "id": "project-1787851299439",
+    "title": "Birthday Celebration 2026",
+    "description": "Owner - DR Nagollagama\nEditor - Alan Crew\nBatch - 2026\nSubject - Physics",
+    "category": "Video Editing",
+    "tags": [
+      "Filmora & Photoshop"
+    ],
+    "startDate": "2026-03-05",
+    "endDate": "2026-03-06",
+    "thumbnail": "https://i.postimg.cc/8PYwtKDS/P5.png",
+    "video": "https://youtu.be/10vvL1QYBeQ?si=DNszvAduMuNYy3-f",
+    "github": "https://github.com/alancreweditz",
+    "demo": "https://youtu.be/10vvL1QYBeQ?si=DNszvAduMuNYy3-f"
+  },
+  {
+    "id": "project-1787853143463",
+    "title": "2026 Revision Begins",
+    "description": "Owner - DR Nagollagama\nEditor - Alan Crew\nBatch - 2026\nSubject - Physics",
+    "category": "Video Editing",
+    "tags": [
+      "Filmora & Photoshop"
+    ],
+    "startDate": "2025-12-14",
+    "endDate": "2025-12-15",
+    "thumbnail": "https://i.postimg.cc/6pJt4NqW/P6.png",
+    "video": "https://youtu.be/cZK85boulM4?si=J9BsxUzX69Ttgc7e",
+    "github": "https://github.com/alancreweditz",
+    "demo": "https://youtu.be/cZK85boulM4?si=J9BsxUzX69Ttgc7e"
+  },
+  {
+    "id": "project-1787853608296",
+    "title": "2025 Exam Guesses",
+    "description": "Owner - DR Nagollagama\nEditor - Alan Crew\nBatch - 2025\nSubject - Physics",
+    "category": "Video Editing",
+    "tags": [
+      "Filmora & Photoshop"
+    ],
+    "startDate": "2025-09-07",
+    "endDate": "2025-09-09",
+    "thumbnail": "https://i.postimg.cc/5tzPHZXD/p7.png",
+    "video": "https://youtu.be/aaP65faUTfc?si=lnuGAZl6VwKGsouZ",
+    "github": "https://github.com/alancreweditz",
+    "demo": "https://youtu.be/aaP65faUTfc?si=lnuGAZl6VwKGsouZ"
+  },
+  {
+    "id": "project-1787854792066",
+    "title": "TRAILER #01",
+    "description": "Owner - DR Nagollagama\nEditor - Alan Crew",
+    "category": "Video Editing",
+    "tags": [
+      "Filmora & After Effects"
+    ],
+    "startDate": "2025-02-26",
+    "endDate": "2025-02-28",
+    "thumbnail": "https://i.postimg.cc/Xvxgy4V2/e1.png",
+    "video": "https://youtu.be/D5At96H62BI",
+    "github": "https://github.com/alancreweditz",
+    "demo": "https://youtu.be/D5At96H62BI"
+  },
+  {
+    "id": "project-1787855313443",
+    "title": "2025 Revision",
+    "description": "Owner - DR Nagollagama\nEditor - Alan Crew\nBatch - 2025\nSubject - Physics",
+    "category": "Video Editing",
+    "tags": [
+      "Filmora & After Effects"
+    ],
+    "startDate": "2025-04-30",
+    "endDate": "2025-05-01",
+    "thumbnail": "https://i.postimg.cc/nhNXRW3p/e2.png",
+    "video": "https://youtu.be/TEAH1wYUvGI",
+    "github": "https://github.com/alancreweditz",
+    "demo": "https://youtu.be/TEAH1wYUvGI"
+  },
+  {
+    "id": "project-1787856224441",
+    "title": "The Film | Movie Edit",
+    "description": "Production - Chineth Cinematic\nDirector - Neshadi\nEditor - Alan Crew",
+    "category": "Video Editing",
+    "tags": [
+      "Filmora"
+    ],
+    "startDate": "2026-03-18",
+    "endDate": "2026-03-27",
+    "thumbnail": "https://i.postimg.cc/T2Qq7Rgx/Final-Edit-mp4-VLC-media-player-8-28-2026-12-09-14-AM.png",
+    "video": "https://l.facebook.com/l.php?u=https%3A%2F%2Fyoutu.be%2FyqYPvNQ7WAo%3Fsi%3Du4KZeHqFbvmKRYW2%26fbclid%3DIwcGRvZgVleHRuA2FlbQIxMABicmlkETF4Q1hjVVR4TTlza2lSSzdXc3J0YwZhcHBfaWQQMjIyMDM5MTc4ODIwMDg5MgABHrBPmXZqei7cwXjoCe0GrxrecEIP0WoSA8hvDLtdvTxBYTwJk9AenQ9ii8Fq_aem_jwweIQ7ZAiRd8_EsqzrZ0g&h=AUBd3Bzc9S7vT1QAbEdSeGVbDudfkCmBRF9zu4UPHLg3Oo21DpNJBPcZWCb6uMuBbmiIAORvJhEocEhMgzyYEPe_9QU-DmCctiDsqmfFbB2afS2aBxkZ2ynvI0PxX_MMS-uq7lBLw4Hd-AnRuWyYlSnM3Q-jU1ocOA&__tn__=-UK-R&c[0]=AUCbtjk8ogpum9wyp3PN2gv8O56Rn17i9RmK4LXV8kVpW6ySITfV2krOd10NONpTHHohwwppQrSwgUcQUUxcbxK9k2X5xFc1o1QrQhIzoIoZdl8eAjNJEKEZOWZ2EcyuinOhgqHPltdJLLW7zG2H_O6rKlMH856TCp_5_3jxVyE9gU-w0bj9yxF5Z1K3AUrPLm3mrTidQH81HzLVpl8DWw",
+    "github": "https://github.com/alancreweditz",
+    "demo": "https://l.facebook.com/l.php?u=https%3A%2F%2Fyoutu.be%2FyqYPvNQ7WAo%3Fsi%3Du4KZeHqFbvmKRYW2%26fbclid%3DIwcGRvZgVleHRuA2FlbQIxMABicmlkETF4Q1hjVVR4TTlza2lSSzdXc3J0YwZhcHBfaWQQMjIyMDM5MTc4ODIwMDg5MgABHrBPmXZqei7cwXjoCe0GrxrecEIP0WoSA8hvDLtdvTxBYTwJk9AenQ9ii8Fq_aem_jwweIQ7ZAiRd8_EsqzrZ0g&h=AUBd3Bzc9S7vT1QAbEdSeGVbDudfkCmBRF9zu4UPHLg3Oo21DpNJBPcZWCb6uMuBbmiIAORvJhEocEhMgzyYEPe_9QU-DmCctiDsqmfFbB2afS2aBxkZ2ynvI0PxX_MMS-uq7lBLw4Hd-AnRuWyYlSnM3Q-jU1ocOA&__tn__=-UK-R&c[0]=AUCbtjk8ogpum9wyp3PN2gv8O56Rn17i9RmK4LXV8kVpW6ySITfV2krOd10NONpTHHohwwppQrSwgUcQUUxcbxK9k2X5xFc1o1QrQhIzoIoZdl8eAjNJEKEZOWZ2EcyuinOhgqHPltdJLLW7zG2H_O6rKlMH856TCp_5_3jxVyE9gU-w0bj9yxF5Z1K3AUrPLm3mrTidQH81HzLVpl8DWw"
+  },
+  {
+    "id": "project-1787886104422",
+    "title": "2028 Batch Begins Short Video",
+    "description": "Owner - DR Nagollagama\nEditor - Alan Crew",
+    "category": "Video Editing | Shorts",
+    "tags": [
+      "Filmora"
+    ],
+    "startDate": "2026-02-26",
+    "endDate": "2026-02-27",
+    "thumbnail": "https://i.postimg.cc/qBwssRBy/p-ROM-1-mp4-VLC-media-player-8-28-2026-8-27-49-AM.png",
+    "video": "https://youtube.com/shorts/0Y3ecyLD0nQ",
+    "github": "https://github.com/alancreweditz",
+    "demo": "https://youtube.com/shorts/0Y3ecyLD0nQ"
+  },
+  {
+    "id": "project-1787886749765",
+    "title": "2025 Guessings ( After Exam)",
+    "description": "Owner - DR Nagollagama\nEditor - Alan Crew",
+    "category": "Video Editing",
+    "tags": [
+      "Filmora | Google Labs"
+    ],
+    "startDate": "2026-01-20",
+    "endDate": "2026-01-21",
+    "thumbnail": "https://i.postimg.cc/yYwydG8V/10-mp4-VLC-media-player-8-28-2026-8-41-09-AM.png",
+    "video": "https://youtu.be/v_stKAwG2BA",
+    "github": "https://github.com/alancreweditz",
+    "demo": "https://youtu.be/v_stKAwG2BA"
+  }
+];
